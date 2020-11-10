678b58893c0efec0d1df4935533bbcdee99d643e
Ankit Batra <ankit.batra@tivo.com>
2020-11-10 17:24:45 +0530

    renamed lifeycle module as wheelie module

--- wheelie/src/androidTest/java/com/abatra/android/wheelie/ExampleInstrumentedTest.java
+++ wheelie/src/androidTest/java/com/abatra/android/wheelie/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.abatra.android.wheelie.lifecycle;
+package com.abatra.android.wheelie;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.abatra.android.wheelie.lifecycle.test", appContext.getPackageName());
+        assertEquals("com.abatra.android.wheelie.test", appContext.getPackageName());
     }
 }
--- wheelie/src/test/java/com/abatra/android/wheelie/ExampleUnitTest.java
+++ wheelie/src/test/java/com/abatra/android/wheelie/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.abatra.android.wheelie.lifecycle;
+package com.abatra.android.wheelie;
 
 import org.junit.Test;
 
