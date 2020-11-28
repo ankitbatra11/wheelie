877c5007b3ef66fbb9f3f245706169146faebb0d
Ankit Batra <ankit.batra@tivo.com>
2020-11-28 21:26:11 +0530

    added a method for setting content intent

--- wheelie/src/main/java/com/abatra/android/wheelie/notification/NotificationBuilder.java
+++ wheelie/src/main/java/com/abatra/android/wheelie/notification/NotificationBuilder.java
@@ -1,19 +1,29 @@
 package com.abatra.android.wheelie.notification;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
 public class NotificationBuilder extends NotificationCompat.Builder {
 
+    private final Context context;
+
     public NotificationBuilder(@NonNull Context context, @NonNull String channelId) {
         super(context, channelId);
+        this.context = context;
     }
 
     public NotificationBuilder headsUpNotification() {
         setPriority(NotificationCompat.PRIORITY_HIGH);
         setVibrate(new long[0]);
+        return this;
+    }
+
+    public NotificationCompat.Builder setContentActivityIntent(Intent intent) {
+        setContentIntent(PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT));
         return this;
     }
 }
