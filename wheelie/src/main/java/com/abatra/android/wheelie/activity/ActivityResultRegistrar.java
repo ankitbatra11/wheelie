08ace5d3c2401a6f9ed097d4e660162e4329e85f
Ankit Batra <ankit.batra@tivo.com>
2020-11-30 00:34:31 +0530

    added context provider method to ActivityResultRegistrar

--- wheelie/src/main/java/com/abatra/android/wheelie/activity/ActivityResultRegistrar.java
+++ wheelie/src/main/java/com/abatra/android/wheelie/activity/ActivityResultRegistrar.java
@@ -1,5 +1,7 @@
 package com.abatra.android.wheelie.activity;
 
+import android.content.Context;
+
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
@@ -7,6 +9,9 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LifecycleOwner;
 
 public interface ActivityResultRegistrar extends LifecycleOwner {
+
     <I, O> ActivityResultLauncher<I> registerForActivityResult(@NonNull final ActivityResultContract<I, O> contract,
                                                                @NonNull final ActivityResultCallback<O> callback);
+
+    Context getContext();
 }
