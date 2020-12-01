98b6f95c343c59fa542d7d0c0da7a31a233cfbf8
Ankit Batra <ankit.batra@tivo.com>
2020-12-01 23:58:40 +0530

    added hashSet observable factory

--- wheelie/src/main/java/com/abatra/android/wheelie/pattern/Observable.java
+++ wheelie/src/main/java/com/abatra/android/wheelie/pattern/Observable.java
@@ -3,6 +3,7 @@ package com.abatra.android.wheelie.pattern;
 import com.abatra.android.wheelie.java8.Consumer;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -15,6 +16,10 @@ public interface Observable<O> {
 
     static <O> Observable<O> synchronizedSet(Set<O> set) {
         return new CollectionObservable<>(Collections.synchronizedSet(set));
+    }
+
+    static <O> Observable<O> hashSet() {
+        return new CollectionObservable<>(new HashSet<>());
     }
 
     void addObserver(O observer);
