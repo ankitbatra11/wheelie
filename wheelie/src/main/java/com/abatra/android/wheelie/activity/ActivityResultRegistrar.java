92a60d8ee40f8ae5251271adc24ddffd791d0f61
Ankit Batra <ankit.batra@tivo.com>
2020-12-02 20:32:45 +0530

    activity result registrar now implements ILifecycleOwner

--- wheelie/src/main/java/com/abatra/android/wheelie/activity/ActivityResultRegistrar.java
+++ wheelie/src/main/java/com/abatra/android/wheelie/activity/ActivityResultRegistrar.java
@@ -1,17 +1,13 @@
 package com.abatra.android.wheelie.activity;
 
-import android.content.Context;
-
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LifecycleOwner;
 
-public interface ActivityResultRegistrar extends LifecycleOwner {
+import com.abatra.android.wheelie.lifecycle.ILifecycleOwner;
 
+public interface ActivityResultRegistrar extends ILifecycleOwner {
     <I, O> ActivityResultLauncher<I> registerForActivityResult(@NonNull final ActivityResultContract<I, O> contract,
                                                                @NonNull final ActivityResultCallback<O> callback);
-
-    Context getContext();
 }
