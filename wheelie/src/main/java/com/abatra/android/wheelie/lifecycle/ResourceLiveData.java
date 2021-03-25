9a36303b32a1f720fddb802da537c53a97f89984
Ankit Batra <ankit.batra@tivo.com>
2021-03-26 01:40:33 +0530

    Added methods to set value from a bolts task that return different result

--- wheelie/src/main/java/com/abatra/android/wheelie/lifecycle/ResourceLiveData.java
+++ wheelie/src/main/java/com/abatra/android/wheelie/lifecycle/ResourceLiveData.java
@@ -2,11 +2,12 @@ package com.abatra.android.wheelie.lifecycle;
 
 import androidx.lifecycle.MutableLiveData;
 
-import com.abatra.android.wheelie.thread.BoltsUtils;
-
 import java.util.Optional;
+import java.util.function.Function;
 
 import bolts.Task;
+
+import static com.abatra.android.wheelie.thread.BoltsUtils.getResult;
 
 class ResourceLiveData<T> implements ResourceLiveDataApi<T> {
 
@@ -17,37 +18,65 @@ class ResourceLiveData<T> implements ResourceLiveDataApi<T> {
         this.liveData = liveData;
     }
 
+    @Override
     public void setResourceValue(T value) {
         liveData.setValue(Resource.loaded(value));
     }
 
+    @Override
     public void postResourceValue(T value) {
         liveData.postValue(Resource.loaded(value));
     }
 
+    @Override
     public void setLoading() {
         liveData.setValue(loading);
     }
 
+    @Override
     public void postLoading() {
         liveData.postValue(loading);
     }
 
+    @Override
     public void setError(Throwable error) {
         liveData.setValue(Resource.failed(error));
     }
 
+    @Override
     public void postError(Throwable error) {
         liveData.postValue(Resource.failed(error));
     }
 
+    @Override
     public void setResource(Task<T> task) {
-        Optional.ofNullable(task.getError()).ifPresent(this::setError);
-        BoltsUtils.getResult(task).ifPresent(this::setResourceValue);
+        setOptionalError(task);
+        getResult(task).ifPresent(this::setResourceValue);
     }
 
+    private void setOptionalError(Task<?> task) {
+        Optional.ofNullable(task.getError()).ifPresent(this::setError);
+    }
+
+    @Override
     public void postResource(Task<T> task) {
+        postOptionalError(task);
+        getResult(task).ifPresent(this::postResourceValue);
+    }
+
+    private void postOptionalError(Task<?> task) {
         Optional.ofNullable(task.getError()).ifPresent(this::postError);
-        BoltsUtils.getResult(task).ifPresent(this::postResourceValue);
+    }
+
+    @Override
+    public <V> void setResource(Task<V> task, Function<V, T> function) {
+        setOptionalError(task);
+        getResult(task).map(function).ifPresent(this::setResourceValue);
+    }
+
+    @Override
+    public <V> void postResource(Task<V> task, Function<V, T> function) {
+        postOptionalError(task);
+        getResult(task).map(function).ifPresent(this::postResourceValue);
     }
 }
